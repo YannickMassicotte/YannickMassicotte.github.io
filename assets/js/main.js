9defa4c08d1a9939cc82360aa0318be402d52017
YannickMassicotte <yanmuzic24@gmail.com>
2020-08-28 11:27:58 -0400

    Add 4 more Vanilla-Js Project, Create Array of Objects(local database) for Projects, Add See Code Button to modal and made it functionnal with the created array

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,76 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+const modals = [
+	{
+		id: 1,
+		name: "bg-color",
+		img: "./images/gif/bg-color.gif",
+		git: "https://github.com/YannickMassicotte/BackgroundColorChanger"
+	},
+	{
+		id: 2,
+		name: "budget",
+		img: "./images/gif/budget.gif",
+		git: "https://github.com/YannickMassicotte/BudgetCalculator"
+	},
+	{
+		id: 3,
+		name: "countdown",
+		img: "./images/gif/countdown.gif",
+		git: "https://github.com/YannickMassicotte/CountDown"
+	},
+	{
+		id: 4,
+		name: "grocery",
+		img: "./images/gif/grocery.gif",
+		git: "https://github.com/YannickMassicotte/GroceryBud"
+	},
+	{
+		id: 5,
+		name: "login",
+		img: "./images/gif/login.gif",
+		git: "https://github.com/YannickMassicotte/Login-Registration"
+	},
+	{
+		id: 6,
+		name: "loremipsum",
+		img: "./images/gif/loremipsum.gif",
+		git: "https://github.com/YannickMassicotte/LoremIpsum"
+	},
+	{
+		id: 7,
+		name: "menu",
+		img: "./images/gif/menu.gif",
+		git: "https://github.com/YannickMassicotte/MenuSortingMeals"
+	},
+	{
+		id: 8,
+		name: "mobshop",
+		img: "./images/gif/mobshop.gif",
+		git: "https://github.com/YannickMassicotte/MobShop"
+	},
+	{
+		id: 9,
+		name: "modal",
+		img: "./images/gif/modal.gif",
+		git: "https://github.com/YannickMassicotte/SimpleModal"
+	},
+	{
+		id: 10,
+		name: "scroll",
+		img: "./images/gif/scroll.gif",
+		git: "https://github.com/YannickMassicotte/ScrollProject"
+	},
+	{
+		id: 11,
+		name: "tabs",
+		img: "./images/gif/tabs.gif",
+		git: "https://github.com/YannickMassicotte/TabsProject5Pillars"
+	},
+
+];
+
 (function($) {
 
 	var	$window = $(window),
